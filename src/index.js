e31e05d2ea6e9e04d2a242ffa6c97089b6533e11
jgumerove <josh.gumerove@gmail.com>
2021-08-09 16:16:52 -0400

    finish adding delete feature

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  
+const form = document.getElementById("create-task-form")
+form.addEventListener("submit", handleSubmit)
+const tasks = document.getElementById("tasks")
+
+// tasks is in global scope 
+
+//handleSubmit is used instead of passing in anonymous function 
+
+function handleSubmit(e) {
+  e.preventDefault()
+  const description = e.target["new-task-description"].value
+  addTask(description)
+  e.target.reset()
+  //debugger
+}
+
+function addTask(description){
+  const task = document.createElement("li")
+  task.innerText = description 
+  const button = document.createElement("button")
+  button.innerText = "delete"
+  button.addEventListener("click", function() {
+    task.remove()
+  })
+  task.appendChild(button)
+  tasks.appendChild(task)
+}
+
+//notice how we divided into steps -- separation of concerns 
+//it is okay for functions to get long 
+//note that button is an element
